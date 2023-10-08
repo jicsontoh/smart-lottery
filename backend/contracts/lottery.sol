999b18bf119161f1a1af0ada97844a73726b56e9
jicsontoh <jicsontoh@gmail.com>
2023-10-08 17:14:31 +0800

    Add lottery smart contract

---
+++ backend/contracts/lottery.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract Lottery {
+    address public manager;
+    address payable[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(payable(msg.sender));
+    }
+}
