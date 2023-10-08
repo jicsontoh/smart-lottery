3e9482bcd21993dc606f263074bd077ac167a8cd
jicsontoh <jicsontoh@gmail.com>
2023-10-08 17:18:39 +0800

    Complete lottery smart contract

--- backend/contracts/lottery.sol
+++ backend/contracts/lottery.sol
@@ -14,4 +14,28 @@ contract Lottery {
         require(msg.value > .01 ether);
         players.push(payable(msg.sender));
     }
+
+    function random() private view returns (uint) {
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(block.prevrandao, block.timestamp, players)
+                )
+            );
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
 }
